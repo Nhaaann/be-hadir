a3aae02cb68988602caed73b089e43ce1ff4073c
Nhaaann <nayhan.nayhn@gmail.com>
2024-09-02 13:23:58 +0700

    update

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -9,13 +9,10 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
         maxWait: 10000,
       },
       errorFormat: 'pretty',
-      datasourceUrl:
-        'postgresql://postgres.fcvhhrzguywmeswewqek:prfVDcjEgBaP1lew@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres',
       log: ['query', 'info', 'warn', 'error'],
       datasources: {
-        
         db: {
-          url: 'postgresql://postgres.fcvhhrzguywmeswewqek:prfVDcjEgBaP1lew@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres',
+          url: 'postgresql://postgres.htuicllcgplhhvvrxxdf:JJaIaIilhOnfqi0F@aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres?pgbouncer=true',
         },
       },
     });
