620a9deb25d4ae9547aca738165b0a8304ad02ae
Nhaaann <nayhan.nayhn@gmail.com>
2024-09-09 12:36:31 +0700

    u

---
+++ src/app/jadwal/initial-schedule.controller.ts
@@ -0,0 +1,29 @@
+// initial-schedule.controller.ts
+import { Controller, Post, Get, Put, Delete, Body, Param } from '@nestjs/common';
+import { InitialScheduleService } from './initial-schedule.service';
+// import { CreateInitialScheduleDto, UpdateInitialScheduleDto } from './initial-schedule.dto';
+
+@Controller('initial-schedule')
+export class InitialScheduleController {
+  constructor(private readonly initialScheduleService: InitialScheduleService) {}
+
+  @Post()
+  async create(@Body() dto: any) {
+    return this.initialScheduleService.createInitialSchedule(dto);
+  }
+
+  @Get()
+  async findAll() {
+    return this.initialScheduleService.getInitialSchedules();
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: number, @Body() dto: any) {
+    return this.initialScheduleService.updateInitialSchedule(id, dto);
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: number) {
+    return this.initialScheduleService.deleteInitialSchedule(id);
+  }
+}
--- src/app/jadwal/jadwal.module.ts
+++ src/app/jadwal/jadwal.module.ts
@@ -3,10 +3,12 @@ import { JadwalController } from './jadwal.controller';
 import { JadwalService } from './jadwal.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { InitialScheduleService } from './initial-schedule.service';
+import { InitialScheduleController } from './initial-schedule.controller';
 
 @Module({
   imports: [PrismaModule],
-  controllers: [JadwalController],
-  providers: [JadwalService],
+  controllers: [JadwalController, InitialScheduleController],
+  providers: [JadwalService, InitialScheduleService],
 })
 export class JadwalModule {}
